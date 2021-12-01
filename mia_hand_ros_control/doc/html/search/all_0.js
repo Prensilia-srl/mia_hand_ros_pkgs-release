c776659179b94d7fa70992f9a753d2605daf57da
frcini <francy.cini@gmail.com>
2021-12-01 10:47:54 +0000

    Trimmed the branch to only the mia_hand_description sub directory

--- mia_hand_ros_control/doc/html/search/all_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['_5f_5finit_5f_5f_0',['__init__',['../classjoint__states__to__float_1_1_joint_states_to_float.html#a0f8b8ef6979aec4c1bb8de94d8ea82c7',1,'joint_states_to_float::JointStatesToFloat']]]
-];
