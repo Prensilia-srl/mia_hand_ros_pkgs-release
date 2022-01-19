3ddd7fee1aa42da903f0003be0fb4e7835e83a27
Andrea Burani <andreabu@prensilia.com>
2022-01-19 13:14:22 +0100

    Trimmed the branch to only the mia_hand_gazebo sub directory

--- mia_hand_description/doc/html/search/all_1.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['chattercallback_1',['chattercallback',['../classmy_remap.html#abcc17c6552dcbf39f6c03f05bd932122',1,'myRemap']]],
-  ['cmake_5fminimum_5frequired_2',['cmake_minimum_required',['../_c_make_lists_8txt.html#a3f5c5f1fcb224f7853595c64b8b74d4c',1,'CMakeLists.txt']]],
-  ['cmakelists_2etxt_3',['CMakeLists.txt',['../_c_make_lists_8txt.html',1,'']]]
-];
--- mia_hand_ros_control/doc/html/files_dup.js
+++
@@ -1,4 +0,0 @@
-var files_dup =
-[
-    [ "mia_hand_ros_control", "dir_1b0ccc368007de3479b136f9056717ef.html", "dir_1b0ccc368007de3479b136f9056717ef" ]
-];
