4e46cba00117ba3af95ef714f3a615006a0132a4
Andrea Burani <andreabu@prensilia.com>
2022-01-19 13:14:31 +0100

    Trimmed the branch to only the mia_hand_bringup sub directory

--- mia_hand_ros_control/doc/html/search/all_2.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['cmake_5fminimum_5frequired_18',['cmake_minimum_required',['../_c_make_lists_8txt.html#a05aaccd54fd894c7fa2491351ad9a196',1,'CMakeLists.txt']]],
-  ['cmakelists_2etxt_19',['CMakeLists.txt',['../_c_make_lists_8txt.html',1,'']]],
-  ['com_5fnumber_5f_20',['COM_number_',['../classmia__hand_1_1_mia_h_w_interface.html#aa8c0e4de64fceaf8ea5f64fd81058c3b',1,'mia_hand::MiaHWInterface']]],
-  ['controlmethod_21',['ControlMethod',['../classmia__hand_1_1_mia_h_w_interface.html#a61ca448009b62dfad84a0c6d9ba62b95',1,'mia_hand::MiaHWInterface']]]
-];
